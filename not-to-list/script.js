e60c9eabad0a0a70177979d57674e79422ccdebb
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-05-26 14:57:38 +1000

    business rules updated

--- not-to-list/script.js
+++ not-to-list/script.js
@@ -3,7 +3,27 @@
 // addBtn.addEventListener("click", () => {
 //   alert("CLICKED");
 // });
+const generateUniqueId = () => {
+  let stringGenerator =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  let stringLength = 6;
+  let stringValue = "";
+
+  for (let i = 0; i < stringLength; i++) {
+    let randomIndex = Math.floor(Math.random() * stringGenerator.length);
+    stringValue += stringGenerator[randomIndex];
+  }
+
+  return stringValue;
+};
+
 let taskList = [];
+
+// let taskList = [
+
+//   { id: generateUniqueId(), task: "Initial Task", hour: 100, type: "good" },
+// ];
 
 const addTask = () => {
   let taskElement = document.getElementById("task");
@@ -12,17 +32,18 @@ const addTask = () => {
 
   //   create the task object
   const taskObject = {
+    id: generateUniqueId(),
     task: taskElement.value,
     hour: parseInt(hourElement.value),
     type: typeElement.value,
   };
 
-  //console.log(taskObject);
+  console.log(taskObject);
 
   //   push the new task to task list
   taskList.push(taskObject);
 
-  //console.log(taskList);
+  console.log(taskList);
 
   //   displaying the updated task list in the ui
   displayTaskList();
@@ -39,36 +60,37 @@ const updateTotalHours = () => {
 };
 
 const updateBadHours = () => {
-  let totalBadHour = taskList.reduce((acc, item) => {
-    if (item.type === "bad") {
-      return acc + parseInt(item.hour);
-    } else {
-      return acc + 0;
-    }
+  let badHour = taskList.reduce((acc, item) => {
+    if (item.type == "bad") return acc + parseInt(item.hour);
+    else return acc + 0;
   }, 0);
-  let totalBadHourElm = document.getElementById("totalBadHour");
 
-  console.log(totalBadHour);
-  totalBadHourElm.innerText = totalBadHour;
+  let badHourElm = document.getElementById("badHour");
+  badHourElm.innerText = badHour;
+  console.log("TOTAL:", badHour);
 };
 
 const displayTaskList = () => {
+  let goodCounter = 0;
+  let badCounter = 0;
   let goodListElm = document.getElementById("goodList");
+
   let trList = "";
   //   loop through taskList
   for (item of taskList) {
     console.log("ITEM", item);
-    if (item.type === "good") {
+    if (item.type == "good") {
+      goodCounter += 1;
       trList =
         trList +
         `<tr>
-            <th scope="row">1</th>
+            <th scope="row">${goodCounter}</th>
             <td>${item.task}</td>
             <td>${item.hour}Hr</td>
             <td>
-                <button type="button" class="btn btn-danger">
+                <button type="button" class="btn btn-danger" onclick="deleteTask('${item.id}')">
                     <i class="fa-solid fa-eraser"></i>
-                </button> <button type="button" class="btn btn-success">
+                </button> <button type="button" class="btn btn-success" onclick="swapTask('${item.id}')">
                     <i class="fa-solid fa-arrow-right"></i>
                 </button>
             </td>
@@ -83,20 +105,24 @@ const displayTaskList = () => {
   let badtrList = "";
   //   loop through taskList
   for (item of taskList) {
-    //console.log("ITEM", item);
-    if (item.type === "bad") {
+    console.log("ITEM", item);
+    if (item.type == "bad") {
+      badCounter += 1;
       badtrList =
         badtrList +
         `<tr>
-            <th scope="row">1</th>
+            <th scope="row">${badCounter}</th>
             <td>${item.task}</td>
             <td>${item.hour}Hr</td>
             <td>
-                <button type="button" class="btn btn-danger">
-                    <i class="fa-solid fa-eraser"></i>
-                </button> <button type="button" class="btn btn-success">
-                    <i class="fa-solid fa-arrow-right"></i>
+                <button type="button" class="btn btn-warning" onclick="swapTask('${item.id}')">
+                    <i class="fa-solid fa-arrow-left"></i>
                 </button>
+
+                <button type="button" class="btn btn-danger" onclick="deleteTask('${item.id}')">
+                    <i class="fa-solid fa-eraser"></i>
+                </button> 
+
             </td>
         </tr>`;
     }
@@ -111,4 +137,30 @@ const displayTaskList = () => {
   updateBadHours();
 };
 
+const deleteTask = (id) => {
+  //alert(id);
+
+  // update the task list without the task with the id
+  taskList = taskList.filter((item) => item.id != id);
+
+  // re render the tasklist
+  displayTaskList();
+};
+
+//swap task list
+const swapTask = (id) => {
+  //alert(id);
+  //update task list by swapping
+  let task = taskList.find((item) => {
+    return item.id == id;
+  });
+
+  if (task.type == "good") {
+    task.type = "bad";
+  } else {
+    task.type = "good";
+  }
+
+  displayTaskList();
+};
 displayTaskList();
